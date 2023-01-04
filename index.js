edfc2954e6e728555a5c974f16a3f1dd99a045e1
nayem9b <razwanniam1@gmail.com>
2023-01-05 05:03:25 +0600

    user login post method added

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dafmrk2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+const usersCollection = client.db("Capital").collection("users");
+app.post("/userInfo", async (req, res) => {
+  const userInfo = req.body;
+  const result = await usersCollection.insertOne(userInfo);
+  res.send(result);
+});
+app.get("/", async (req, res) => {
+  res.send("Homepage is working");
+});
+app.listen(port, () => {
+  console.log(`my app is running on ${port}`);
+});
